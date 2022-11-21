45e2ba3562485946f28f41d7b386170c180c92a2
Özgür ÖZALP <ozgur@altogic.com>
2022-11-21 12:38:19 +0300

    updated

--- components/GenerateQR.tsx
+++ components/GenerateQR.tsx
@@ -1,16 +1,20 @@
 import { SpinnerCircular } from 'spinners-react';
-import QRCode from 'react-qr-code';
 import { QRCodeSVG } from 'qrcode.react';
 // @ts-ignore
 import { saveSvgAsPng } from 'save-svg-as-png';
 import { FormEvent, useRef, useState } from 'react';
+import html2canvas from 'html2canvas';
 
 export default function GenerateQR() {
 	const [value, setValue] = useState('');
 	const [loading, setLoading] = useState(false);
 	const input = useRef<HTMLInputElement>(null);
+	const shareTarget = useRef<HTMLElement>(null);
+
 	const download = () => {
-		saveSvgAsPng(document.getElementById('QRCode'), 'diagram.png', {
+		const svg = document.getElementById('QRCode');
+		if (!svg) return;
+		saveSvgAsPng(svg, 'altogic-qrcode.png', {
 			scale: 10,
 		});
 	};
@@ -25,10 +29,28 @@ export default function GenerateQR() {
 	};
 
 	const configureQRValue = (value: string) => {
-		const url = 'http://localhost:3000';
-		console.log(`${url}/${value}`);
-		setValue(`${url}/read-qr/${value}`);
+		const url = new URL(window.location.href);
+		console.log(`${url.origin}/read-qr/${value}`);
+		setValue(`${url.origin}/read-qr/${value}`);
 	};
+
+	async function shareImage() {
+		const svg = document.getElementById('QRCode');
+		if (!svg) return;
+		const canvas = await html2canvas(svg);
+		const dataUrl = canvas.toDataURL();
+		const blob = await (await fetch(dataUrl)).blob();
+		const filesArray = [
+			new File([blob], 'altogic-qrcode.png', {
+				type: 'image/png',
+				lastModified: new Date().getTime(),
+			}),
+		];
+		const shareData = {
+			files: filesArray,
+		};
+		await navigator.share(shareData);
+	}
 
 	return (
 		<>
@@ -45,7 +67,7 @@ export default function GenerateQR() {
 				<button
 					disabled={loading}
 					type="submit"
-					className="shrink-0 border h-12 overflow-hidden relative transition border-gray-500 px-3 h-full ring-2 rounded ring-transparent outline-none focus:active:ring-black active:ring-black ring-offset-2"
+					className="shrink-0 border h-full overflow-hidden relative transition border-gray-500 px-3 h-full ring-2 rounded ring-transparent outline-none focus:active:ring-black active:ring-black ring-offset-2"
 				>
 					QR Oluştur
 					{loading && (
@@ -55,24 +77,32 @@ export default function GenerateQR() {
 					)}
 				</button>
 			</form>
-			<div className="flex flex-col gap-6 items-center justify-center">
+			<div className="flex flex-col gap-3 sm:gap-6 items-center justify-center">
 				{value && (
 					<>
 						<div className="bg-green-600 w-full p-3 text-white rounded">
-							<h3 className="text-2xl">QR Kodunuz Oluşturulmuştur.</h3>
+							<h3 className="text-xl sm:text-2xl">QR Kodunuz Oluşturulmuştur.</h3>
 							<div className="border-b border-white/40 h-[1px] my-2" />
-							<div className="text-justify">
+							<div className="text-base sm:text-lg text-justify">
 								Lütfen QR kodunuzu ekran görüntüsü alarak veya aşağıdaki indirme butonuna tıklayarak
 								kaydediniz. Aksi takdirde QR kodunuz silinecektir ve tekrardan oluşturulamayacaktır.
 							</div>
 						</div>
-						<QRCodeSVG className="w-full h-full md:w-[512px]" value={value} />
+						<QRCodeSVG id="QRCode" className="w-full h-full md:w-[512px]" value={value} />
 						<button
 							className="w-full border min-h-[50px] transition border-gray-500 px-3 ring-2 rounded ring-transparent outline-none focus:active:ring-black active:ring-black ring-offset-2"
 							onClick={download}
 						>
 							QR Kodunu İndir
 						</button>
+						{'share' in navigator && (
+							<button
+								className="w-full border min-h-[50px] transition border-gray-500 px-3 ring-2 rounded ring-transparent outline-none focus:active:ring-black active:ring-black ring-offset-2"
+								onClick={shareImage}
+							>
+								QR Kodunu Paylaş
+							</button>
+						)}
 					</>
 				)}
 			</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import GenerateQR from '../components/GenerateQR';
 
 export default function Home() {
 	return (
-		<section className="flex flex-col gap-6 py-6">
+		<section className="flex flex-col gap-3 sm:gap-6 py-6">
 			<Logo className="h-16" />
 			<GenerateQR />
 		</section>
